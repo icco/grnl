d659640c7a85d49ea382a51338a7552204b86883
Nat Welch <nat@natwelch.com>
2014-12-28 21:49:23 +0000

    no need

--- grnl.go
+++ grnl.go
@@ -3,25 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 )
 
 func main() {
-
-	// `os.Args` provides access to raw command-line
-	// arguments. Note that the first value in this slice
-	// is the path to the program, and `os.Args[1:]`
-	// holds the arguments to the program.
-	argsWithProg := os.Args
-	argsWithoutProg := os.Args[1:]
-
-	// You can get individual args with normal indexing.
-	arg := os.Args[3]
-
-	fmt.Println(argsWithProg)
-	fmt.Println(argsWithoutProg)
-	fmt.Println(arg)
-
 	// Basic flag declarations are available for string,
 	// integer, and boolean options. Here we declare a
 	// string flag `word` with a default value `"foo"`
